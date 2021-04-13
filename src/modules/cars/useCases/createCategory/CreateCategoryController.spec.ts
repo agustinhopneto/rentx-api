9c3112162947eeb06952acb6d53f309416ffcddd
Agustinho Neto <agustinho.pneto@gmail.com>
2021-04-13 18:33:36 -0300

    Add integration tests

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,7 +1,95 @@
+import { hash } from 'bcrypt';
 import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuid } from 'uuid';
 
 import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
 
-describe('Create Category Controller', async () => {
-  await request(app).get('/cars/available').expect(201);
+let connection: Connection;
+
+describe('Create Category Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const password = await hash('admin1234', 8);
+
+    await connection.query(
+      `INSERT INTO users(id, name, email, password, is_admin, created_at, driver_license)
+      VALUES('${id}', 'Administrador', 'admin@rentx.com', '${password}', 'true', 'now()', 'XXXXXX')`,
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should not be able to create a category with an existent name', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com',
+      password: 'admin1234',
+    });
+
+    const { token } = responseToken.body;
+
+    await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest',
+        description: 'Description supertest',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest',
+        description: 'Description supertest',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should be able to list all categories', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com',
+      password: 'admin1234',
+    });
+
+    const { token } = responseToken.body;
+
+    await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest',
+        description: 'Description supertest',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest 2',
+        description: 'Description supertest 2',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    const response = await request(app).get('/categories');
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(2);
+    expect(response.body[0]).toHaveProperty('id');
+  });
 });
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,7 +5,11 @@ export default async (host = 'rentx_postgres'): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'rentx_test'
+          : defaultOptions.database,
     }),
   );
 };
